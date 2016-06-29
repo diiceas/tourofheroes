dbf84bac201ce752ba3781f1858378ea92ec2025
diiceas <forthepost@gmail.com>
2016-06-29 11:48:08 +0200

    add two way databinding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
-    @Component({
-        selector: 'my-app',
-        template: '<h1> First Angular 2 App</h1>'
-        })
+export class Hero {
+    id: number;
+    name: string;
+}
 
-export class AppComponent { }
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+        `
+})
+
+export class AppComponent {
+    title = 'Tour of heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
