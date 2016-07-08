ae5140d23beceb0ec7fdd5b8322f57304b378693
diiceas <forthepost@gmail.com>
2016-07-08 13:23:28 +0300

    add delay of 2 seconds for loading heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -66,16 +66,16 @@ import { HeroService } from './hero.service';
     directives: [HeroDetailComponent],
     providers: [HeroService]
 })
-export class AppComponent implements OnInit{
-  title = 'Tour of Heroes';
+export class AppComponent implements OnInit {
+    title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
-    constructor(private heroService: HeroService){}
+    constructor(private heroService: HeroService) { }
     getHeroes() {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
     }
-    ngOnInit(){
-        this.getHeroes();   
+    ngOnInit() {
+        this.getHeroes();
     }
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
--- app/hero.service.js
+++ app/hero.service.js
@@ -13,8 +13,10 @@ var mock_heroes_1 = require('./mock-heroes');
 var HeroService = (function () {
     function HeroService() {
     }
-    HeroService.prototype.getHeroes = function () {
-        return Promise.resolve(mock_heroes_1.HEROES);
+    HeroService.prototype.getHeroesSlowly = function () {
+        return new Promise(function (resolve) { return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000); } //2 seconds
+         //2 seconds
+        );
     };
     HeroService = __decorate([
         core_1.Injectable(), 
